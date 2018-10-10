233bc8bd0115eb9f8ef57b48e17415471fd20dd6
ivanjunckes <ijunckes@tomitribe.com>
2018-10-09 23:20:36 -0300

    Fix client test

--- inget-client-java/src/test/resources/movies/expected/io/superbiz/video/rest/client/MovieClient.java
+++ inget-client-java/src/test/resources/movies/expected/io/superbiz/video/rest/client/MovieClient.java
@@ -2,6 +2,7 @@ package io.superbiz.video.rest.client;
 
 import io.superbiz.video.rest.client.base.ClientConfiguration;
 import io.superbiz.video.rest.client.base.MovieClientExceptionMapper;
+import io.superbiz.video.rest.client.interfaces.MovieResourceBeanClient;
 import javax.annotation.Generated;
 import org.apache.johnzon.jaxrs.JohnzonProvider;
 import org.eclipse.microprofile.rest.client.RestClientBuilder;
@@ -9,9 +10,22 @@ import org.eclipse.microprofile.rest.client.RestClientBuilder;
 @Generated("org.tomitribe.inget.client.ClientGenerator")
 public class MovieClient {
 
+    private MovieResourceBeanClient movieResourceBeanClient;
+
     public MovieClient(
             ClientConfiguration config) {
         RestClientBuilder builder = RestClientBuilder.newBuilder().baseUrl(config.getUrl())
                 .register(JohnzonProvider.class).register(MovieClientExceptionMapper.class);
+        if (config.getSignature() != null) {
+            builder.register(new io.superbiz.video.rest.client.base.SignatureAuthenticator(config));
+        }
+        if (config.getBasic() != null) {
+            builder.register(new io.superbiz.video.rest.client.base.BasicAuthenticator(config));
+        }
+        movieResourceBeanClient = builder.build(MovieResourceBeanClient.class);
+    }
+
+    public MovieResourceBeanClient movie() {
+        return this.movieResourceBeanClient;
     }
 }
